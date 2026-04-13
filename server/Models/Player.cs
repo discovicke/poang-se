70513c228cc8fc0929348369bad0982e2298a2a9
discotomten <linkaan_lonn@hotmail.com>
2026-04-13 13:39:38 +0200

    add null safety

--- server/Models/Player.cs
+++ server/Models/Player.cs
@@ -1,7 +1,7 @@
 public class Player
 {
     public Guid Id { get; set; }
-    public string UserName { get; set; }
+    public string UserName { get; set; } = "";
     public DateTime CreatedAt { get; set; }
-    public List<Score> Scores { get; set; }
+    public List<Score>? Scores { get; set; }
 }
