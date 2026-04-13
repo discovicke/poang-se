1fe7c4f654935eca15d9a38e7b3838bc5148405e
jespernapp <jesperooo@live.se>
2026-04-13 12:40:50 +0200

    Add score.cs

---
+++ server/Models/Score.cs
@@ -0,0 +1,7 @@
+public class Score
+{
+    public Guid Id { get; set; }
+    public Guid PlayerId { get; set; }
+    public double Value { get; set; }
+    public DateTime CreatedAt { get; set; } 
+}
