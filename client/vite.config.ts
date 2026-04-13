39462c41af7441b1df9f6e05e425050da6c82cc7
discotomten <linkaan_lonn@hotmail.com>
2026-04-13 10:01:50 +0200

    add proxy, need .env file for it to work in client

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  })
+}
