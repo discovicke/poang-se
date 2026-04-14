004a2a24c2979c498edf3772c2e4700ce124c226
disco <discovicke@gmail.com>
2026-04-14 10:40:07 +0200

    Add DTOs for game creation, team addition, player management, and scoring

---
+++ server/Dtos/GameDto.cs
@@ -0,0 +1,28 @@
+﻿namespace server.Dtos;
+
+public class CreateGameDto
+{
+    public string Name { get; set; } = "";
+    public bool LowerIsBetter { get; set; } = false;
+    public int? MaxRounds { get; set; } = 1;
+}
+
+public class AddTeamDto
+{
+    public string Name { get; set; } = "";
+}
+
+public class AddPlayerToGameDto
+{
+    public Guid PlayerId { get; set; }
+    public Guid? TeamId { get; set; }
+}
+
+public class AddScoreToGameDto
+{
+    public Guid PlayerId { get; set; }
+    public Guid? TeamId { get; set; }
+    public int? Round { get; set; }
+    public double Value { get; set; }
+}
+
