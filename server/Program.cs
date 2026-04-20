df482465dfe56dad3f4683b055e5eb71889bd8fc
jespernapp <jesperooo@live.se>
2026-04-20 18:26:41 +0200

    Fix program.cs to not load local env in prod

--- server/Program.cs
+++ server/Program.cs
@@ -6,8 +6,11 @@ using server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var envPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", ".env"));
-Env.Load(envPath);
+if (builder.Environment.IsDevelopment())
+{
+    var envPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", ".env"));
+    Env.Load(envPath);
+}
 
 var connString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? "";
 
